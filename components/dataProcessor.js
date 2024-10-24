8f844091b8199ec6f299f7cb05b85799ffa1cb50
jhoffmann <jhoffmann@adobe.com>
2024-10-24 12:18:49 +0200

    changed dataProcessor.js to imperative code style DO NOT MERGE WITH MAIN!!!

--- components/dataProcessor.js
+++ components/dataProcessor.js
@@ -1,115 +1,146 @@
 import rawData from '../public/employment-data.json'
 
-const processCategory = (category) =>
-    Object.entries(category.index)
-        .map(([id, index]) => ({
-          id,
-          label: category.label[id],
-          index: parseInt(index)
-        }))
-        .sort((a, b) => a.index - b.index)
-
-const processData = (data) => {
-  const { dataset } = data
-  const { Grossregion, Wirtschaftssektor, Geschlecht, Quartal } = dataset.dimension
-
-  return {
-    regions: processCategory(Grossregion.category),
-    sectors: processCategory(Wirtschaftssektor.category),
-    genders: processCategory(Geschlecht.category),
-    quarters: processCategory(Quartal.category),
-    employmentData: dataset.value,
-    dimensions: {
-      regionsCount: Object.keys(Grossregion.category.index).length,
-      sectorsCount: Object.keys(Wirtschaftssektor.category.index).length,
-      gendersCount: Object.keys(Geschlecht.category.index).length,
-      quartersCount: Object.keys(Quartal.category.index).length
+class DataProcessor {
+    constructor() {
+        this.rawData = rawData
+        this.processedData = this.processData()
     }
-  }
-}
 
-const getIndex = (processedData, regionId, sectorId, genderId, quarterId) => {
-  const { Grossregion, Wirtschaftssektor, Geschlecht, Quartal } = rawData.dataset.dimension
-  const { sectorsCount, gendersCount, quartersCount } = processedData.dimensions
+    processData() {
+        const { dataset } = this.rawData
+        const { Grossregion, Wirtschaftssektor, Geschlecht, Quartal } = dataset.dimension
 
-  const regionIndex = parseInt(Grossregion.category.index[regionId])
-  const sectorIndex = parseInt(Wirtschaftssektor.category.index[sectorId])
-  const genderIndex = parseInt(Geschlecht.category.index[genderId])
-  const quarterIndex = parseInt(Quartal.category.index[quarterId])
+        return {
+            regions: this.processCategory(Grossregion.category),
+            sectors: this.processCategory(Wirtschaftssektor.category),
+            genders: this.processCategory(Geschlecht.category),
+            quarters: this.processCategory(Quartal.category),
+            employmentData: dataset.value,
+            dimensions: {
+                regionsCount: Object.keys(Grossregion.category.index).length,
+                sectorsCount: Object.keys(Wirtschaftssektor.category.index).length,
+                gendersCount: Object.keys(Geschlecht.category.index).length,
+                quartersCount: Object.keys(Quartal.category.index).length
+            }
+        }
+    }
 
-  return (
-      regionIndex * sectorsCount * gendersCount * quartersCount +
-      sectorIndex * gendersCount * quartersCount +
-      genderIndex * quartersCount +
-      quarterIndex
-  )
-}
-
-const getGenderDistribution = (processedData) => (regionId, sectorId, quarterId) => {
-  const maleIndex = getIndex(processedData, regionId, sectorId, "1", quarterId)
-  const femaleIndex = getIndex(processedData, regionId, sectorId, "2", quarterId)
-
-  const maleValue = processedData.employmentData[maleIndex] || 0
-  const femaleValue = processedData.employmentData[femaleIndex] || 0
-
-  const total = maleValue + femaleValue
-
-  return {
-    male: maleValue,
-    female: femaleValue,
-    malePercentage: total > 0 ? (maleValue / total * 100).toFixed(1) : '0.0',
-    femalePercentage: total > 0 ? (femaleValue / total * 100).toFixed(1) : '0.0',
-    total
-  }
-}
-
-const getGenderTrend = (processedData) => (regionId, sectorId) =>
-    processedData.quarters.map(quarter => {
-      const distribution = getGenderDistribution(processedData)(regionId, sectorId, quarter.id)
-      return {
-        quarter: quarter.label,
-        male: distribution.male,
-        female: distribution.female,
-        malePercentage: distribution.malePercentage,
-        femalePercentage: distribution.femalePercentage
-      }
-    })
-
-const getSectorComparison = (processedData) => (regionId, quarterId) =>
-    processedData.sectors
-        .filter(sector => sector.id !== 'TOT')
-        .map(sector => {
-          const distribution = getGenderDistribution(processedData)(regionId, sector.id, quarterId)
-          return {
-            sector: sector.label,
-            malePercentage: distribution.malePercentage,
-            femalePercentage: distribution.femalePercentage,
-            total: distribution.total
-          }
+    processCategory(category) {
+        const result = []
+        for (let id in category.index) {
+            result.push({
+                id: id,
+                label: category.label[id],
+                index: parseInt(category.index[id])
+            })
+        }
+        result.sort(function(a, b) {
+            return a.index - b.index
         })
+        return result
+    }
 
-const getLatestQuarter = (processedData) =>
-    processedData.quarters[processedData.quarters.length - 1].id
+    getIndex(regionId, sectorId, genderId, quarterId) {
+        const { Grossregion, Wirtschaftssektor, Geschlecht, Quartal } = this.rawData.dataset.dimension
+        const { sectorsCount, gendersCount, quartersCount } = this.processedData.dimensions
 
-const testDataPoint = (processedData) => {
-  const distribution = getGenderDistribution(processedData)("0", "3", "2024Q2")
-  console.log("Test Data Point (Schweiz, Sektor 3, 2024Q2):")
-  console.log(`Männer: ${distribution.male} (${distribution.malePercentage}%)`)
-  console.log(`Frauen: ${distribution.female} (${distribution.femalePercentage}%)`)
-  console.log(`Total: ${distribution.total}`)
+        const regionIndex = parseInt(Grossregion.category.index[regionId])
+        const sectorIndex = parseInt(Wirtschaftssektor.category.index[sectorId])
+        const genderIndex = parseInt(Geschlecht.category.index[genderId])
+        const quarterIndex = parseInt(Quartal.category.index[quarterId])
+
+        return (
+            regionIndex * sectorsCount * gendersCount * quartersCount +
+            sectorIndex * gendersCount * quartersCount +
+            genderIndex * quartersCount +
+            quarterIndex
+        )
+    }
+
+    getRegions() {
+        return this.processedData.regions
+    }
+
+    getSectors() {
+        return this.processedData.sectors
+    }
+
+    getQuarters() {
+        return this.processedData.quarters
+    }
+
+    getGenderDistribution(regionId, sectorId, quarterId) {
+        const maleIndex = this.getIndex(regionId, sectorId, "1", quarterId)
+        const femaleIndex = this.getIndex(regionId, sectorId, "2", quarterId)
+
+        const maleValue = this.processedData.employmentData[maleIndex] || 0
+        const femaleValue = this.processedData.employmentData[femaleIndex] || 0
+
+        const total = maleValue + femaleValue
+
+        let malePercentage = '0.0'
+        let femalePercentage = '0.0'
+
+        if (total > 0) {
+            malePercentage = (maleValue / total * 100).toFixed(1)
+            femalePercentage = (femaleValue / total * 100).toFixed(1)
+        }
+
+        return {
+            male: maleValue,
+            female: femaleValue,
+            malePercentage: malePercentage,
+            femalePercentage: femalePercentage,
+            total: total
+        }
+    }
+
+    getGenderTrend(regionId, sectorId) {
+        const trend = []
+        for (let i = 0; i < this.processedData.quarters.length; i++) {
+            const quarter = this.processedData.quarters[i]
+            const distribution = this.getGenderDistribution(regionId, sectorId, quarter.id)
+            trend.push({
+                quarter: quarter.label,
+                male: distribution.male,
+                female: distribution.female,
+                malePercentage: distribution.malePercentage,
+                femalePercentage: distribution.femalePercentage
+            })
+        }
+        return trend
+    }
+
+    getSectorComparison(regionId, quarterId) {
+        const comparison = []
+        for (let i = 0; i < this.processedData.sectors.length; i++) {
+            const sector = this.processedData.sectors[i]
+            if (sector.id !== 'TOT') {
+                const distribution = this.getGenderDistribution(regionId, sector.id, quarterId)
+                comparison.push({
+                    sector: sector.label,
+                    malePercentage: distribution.malePercentage,
+                    femalePercentage: distribution.femalePercentage,
+                    total: distribution.total
+                })
+            }
+        }
+        return comparison
+    }
+
+    getLatestQuarter() {
+        return this.processedData.quarters[this.processedData.quarters.length - 1].id
+    }
+
+    testDataPoint() {
+        const distribution = this.getGenderDistribution("0", "3", "2024Q2")
+        console.log("Test Data Point (Schweiz, Sektor 3, 2024Q2):")
+        console.log("Männer: " + distribution.male + " (" + distribution.malePercentage + "%)")
+        console.log("Frauen: " + distribution.female + " (" + distribution.femalePercentage + "%)")
+        console.log("Total: " + distribution.total)
+    }
 }
 
-const processedData = processData(rawData)
-
-const dataProcessor = {
-  getRegions: () => processedData.regions,
-  getSectors: () => processedData.sectors,
-  getQuarters: () => processedData.quarters,
-  getGenderDistribution: getGenderDistribution(processedData),
-  getGenderTrend: getGenderTrend(processedData),
-  getSectorComparison: getSectorComparison(processedData),
-  getLatestQuarter: () => getLatestQuarter(processedData),
-  testDataPoint: () => testDataPoint(processedData)
-}
-
+const dataProcessor = new DataProcessor()
+dataProcessor.testDataPoint()
 export default dataProcessor
